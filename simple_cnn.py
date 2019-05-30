bf0d6984ecd1fd0b597fcf14fc841599ebade46e
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2019-05-30 13:20:09 +0300

    Change dataset parser to generator

---
+++ simple_cnn.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import keras.callbacks
+import tensorflow.contrib.eager as tfe
+
+from keras.utils import np_utils
+import tensorflow.python.keras.models as models
+from tensorflow.python.keras.layers.core import Reshape,Dense,Dropout,Activation,Flatten
+from tensorflow.python.keras.layers.noise import GaussianNoise
+from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+from tensorflow.python.keras.regularizers import *
+from tensorflow.python.keras.optimizers import adam
+
+from utils import deepsig_dataset_generator as dp
+
+
+ds = tf.data.Dataset.from_generator(
+    dp.deepsig_dataset_generator('/home/ctriant/deepsig-dataset/2018.01/', snr=[10]), 
+    (tf.float32, tf.uint8),
+    ([2,1024],[]))
+
+in_shp = ds.output_shapes[0].as_list()
+
+dr = 0.5 # dropout rate (%)
+model = models.Sequential()
+model.add(Reshape([1]+in_shp, input_shape=in_shp))
+model.add(ZeroPadding2D((0, 2)))
+model.add(Conv2D(256, (1, 3), activation="relu", name="conv1", padding="valid", kernel_initializer="glorot_uniform"))
+model.add(Dropout(dr))
+model.add(ZeroPadding2D((0, 2)))
+model.add(Conv2D(256, (1, 3), activation="relu", name="conv12", padding="valid", kernel_initializer="glorot_uniform"))
+model.add(Dropout(dr))
+model.add(Flatten())
+model.add(Dense(256, activation='relu', name="dense1"))
+model.add(Dropout(dr))
+model.add(Dense( 24, name="dense2" ))
+model.add(Activation('softmax'))
+model.add(Reshape([24]))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.summary()
+
+
+#Set up some params 
+nb_epoch = 100     # number of epochs to train on
+batch_size = 1  # training batch size
+dataset = ds.shuffle(buffer_size=5000)
+dataset = dataset.batch(batch_size)
+dataset = dataset.repeat()
+# dataset = dataset.prefetch(buffer_size=batch_size)
+
+# filepath = 'convmodrecnets_CNN2_0.5.wts.h5'
+history = model.fit(dataset, epochs=10, steps_per_epoch=32)
+# model.load_weights(filepath)
---
+++ utils/deepsig_dataset_generator.py
@@ -0,0 +1,70 @@
+import numpy as np
+import h5py
+
+import os
+import errno
+import random
+import sys
+import threading
+
+import tensorflow as tf
+
+
+
+class deepsig_dataset_generator():
+    """A toolkit for parsing the Deepsig Inc. 2018.01 dataset"""
+
+    def __init__(self, dataset_path, modulation=None, snr=None):
+        self.dataset_path = dataset_path
+        self.dataset = h5py.File(dataset_path+"/GOLD_XYZ_OSC.0001_1024.hdf5", 'r')
+        self.__init_data()
+        self.__init_modulations()
+        self.__init_snr()
+        self.total_samples_num = self.modulations[:,0].size
+        self.modulations_num = self.modulations[0,:].size
+        self.snr_num = 26
+        self.mod_classes = self.list_modulations()
+        self.modarg = modulation
+        self.snrarg = snr
+        
+    def __init_data(self):
+        self.data = self.dataset['X']
+    
+    def __init_modulations(self):
+        self.modulations = self.dataset['Y']
+        
+    def __init_snr(self):
+        self.snr = self.dataset['Z']
+
+    def __get_index(self, occur, l):
+        return [l.index(idx.upper()) for idx in occur]
+        
+    def list_modulations(self):
+        f = open(self.dataset_path+'/classes.txt', 'r')
+        s = f.read()
+        class_list = s.split("',\n '")
+        class_list[0] = class_list[0].split("classes = ['")[1]
+        class_list[-1] = class_list[-1].split("']\n")[0]
+        return class_list
+    
+    '''
+    Get data from the dataset.
+    '''
+    def __call__(self):
+        ind = []
+        labels = []
+        
+        if self.snrarg is None:
+            self.snrarg = [i for i in range(-20,32,2)]
+                
+        if self.modarg is None:
+            self.modarg = self.list_modulations()
+        
+        mods = self.__get_index(self.modarg, self.mod_classes)
+        for column in (self.snr[:] == self.snrarg).T:
+            q = np.where(column & 
+                        (self.modulations[:,mods] == 1).any(axis=1))[0]
+            indx = (np.array(q).reshape(-1,1))
+            for i in range(0,indx.shape[0]):
+                yield (self.data[indx[i],:,:][0].transpose(), np.argmax(self.modulations[indx[i],:][0]))
+ 
