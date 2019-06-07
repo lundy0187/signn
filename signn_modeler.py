350c3567d1c38a89345ae78412f6b14ae20e77be
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2019-06-07 13:40:53 +0300

    Fix flake8 syntax errors

--- signn_modeler.py
+++ signn_modeler.py
@@ -1,33 +1,23 @@
 import argparse
-import os
-import errno
-import sys
-
-from enum import Enum
-
-import tensorflow as tf
 import tensorflow.python.keras.models as models
+from tensorflow.python.keras.layers.core import (Reshape, Dense, Dropout,
+                                                 Activation, Flatten)
+from tensorflow.python.keras.layers.convolutional import (Conv2D,
+                                                          ZeroPadding2D)
 
-from tensorflow.python.keras.utils import np_utils
-from tensorflow.python.keras.layers.core import Reshape, Dense, Dropout, Activation, Flatten
-from tensorflow.python.keras.layers.noise import GaussianNoise
-from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
-from tensorflow.python.keras.regularizers import *
-from tensorflow.python.keras.optimizers import adam 
 
-        
 class signn_modeler():
-        
+
     def __init__(self, model, input_shape, destination):
         self.model_choice = model
         self.input_shape = input_shape
         self.model = self.get_model()
         self.destination = destination
-        
+
     def get_model(self):
         if (self.model_choice == "deepsig"):
             return self.get_deepsig_cnn()
-    
+
     def get_deepsig_cnn(self):
         dr = 0.5
         model = models.Sequential()
@@ -43,42 +33,49 @@ class signn_modeler():
         model.add(Flatten())
         model.add(Dense(256, activation='relu', name="dense1"))
         model.add(Dropout(dr))
-        model.add(Dense( 24, name="dense2" ))
+        model.add(Dense(24, name="dense2"))
         model.add(Activation('softmax'))
         model.add(Reshape([24]))
         model.compile(loss='categorical_crossentropy', optimizer='adam')
         return model
-        
+
     def export_model(self):
         self.model.save(self.destination)
-        
+
     def print_model_summary(self):
         self.model.summary()
-        
+
+
 def argument_parser():
     description = 'A tool to generate models using Keras/Tensorflow'
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                     description=description)
-    parser.add_argument("-m", "--model", default='deepsig', nargs='?', dest="model",
-                        action="store", choices=['deepsig'],
-                        help='Choose the model to generate. (Default: %(default)s)')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=description)
+    parser.add_argument("-m", "--model", default='deepsig', nargs='?',
+                        dest="model", action="store", choices=['deepsig'],
+                        help='Choose the model to generate. \
+                            (Default: %(default)s)')
     parser.add_argument("-i", "--input-shape", dest="input_shape", nargs='+',
-                        type=int, help='Set the model\'s input shape', required=True)
+                        type=int, help='Set the model\'s input shape',
+                        required=True)
     parser.add_argument("-s", "--save", dest="destination", action='store',
                         help="Export the generated model at the given path.")
     parser.add_argument("-v", "--verbose", dest="verbose", action='store_true',
                         help="Print info regarding the generated model.")
     return parser
 
+
 def main(modeler=signn_modeler, args=None):
     if args is None:
         args = argument_parser().parse_args()
 
-    m = modeler(model=args.model, input_shape=args.input_shape, destination=args.destination)
+    m = modeler(model=args.model, input_shape=args.input_shape,
+                destination=args.destination)
     if (args.destination):
         m.export_model()
     if (args.verbose):
         m.print_model_summary()
+
 
 if __name__ == '__main__':
     main()
--- signn_trainer.py
+++ signn_trainer.py
@@ -1,18 +1,18 @@
 import argparse
 import os
 import errno
-import math
 import time
 import tensorflow as tf
 import tensorflow.python.keras.models as models
-from tensorflow.python.keras.utils import np_utils
 
 from utils import deepsig_dataset_generator as ddg
 
+
 class signn_trainer():
-    
-    def __init__(self, dataset_path, model_path, epochs, steps_per_epoch, batch_size,
-                 shuffle, shuffle_buffer_size, split_ratio, validation_steps):
+
+    def __init__(self, dataset_path, model_path, epochs, steps_per_epoch,
+                 batch_size, shuffle, shuffle_buffer_size, split_ratio,
+                 validation_steps):
         self.batch_size = batch_size
         self.epochs = epochs
         self.steps_per_epoch = steps_per_epoch
@@ -22,66 +22,73 @@ class signn_trainer():
         self.validation_steps = validation_steps
         self.dataset_path = dataset_path
         self.model_path = model_path
-        self.dataset_parser = ddg.deepsig_dataset_generator(self.dataset_path, snr=[10])
-        self.train_samples = int((self.split_ratio[0]*self.dataset_parser.get_total_samples()/self.batch_size))
-        self.validation_samples = int((self.split_ratio[1]*self.dataset_parser.get_total_samples()/self.batch_size))
+        self.dataset_parser = ddg.deepsig_dataset_generator(self.dataset_path,
+                                                            snr=[10])
+        self.train_samples = int((self.split_ratio[0]*self.dataset_parser
+                                 .get_total_samples()/self.batch_size))
+        self.validation_samples = int((self.split_ratio[1]*self.dataset_parser
+                                       .get_total_samples()/self.batch_size))
         self.__init_dataset()
         self.model = self.__init_model()
 
     def __init_dataset(self):
-        #TODO: Fix the dataset initialization
+        # TODO: Fix the dataset initialization
         if (not os.path.exists(self.dataset_path)):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
                                     self.dataset_path)
         dataset = tf.data.Dataset.from_generator(
             self.dataset_parser,
-            (tf.float32, tf.uint8), ([2,1024],[]))
+            (tf.float32, tf.uint8), ([2, 1024], []))
         if self.shuffle:
             dataset = dataset.shuffle(buffer_size=self.shuffle_buffer_size,
                                       seed=int(round(time.time() * 1000)),
                                       reshuffle_each_iteration=False)
         dataset = dataset.batch(batch_size=self.batch_size)
         self.validation_dataset = dataset.take(self.validation_samples)
-        self.validation_dataset = self.validation_dataset.prefetch(buffer_size=self.batch_size)
+        self.validation_dataset = self.validation_dataset.prefetch(
+            buffer_size=self.batch_size)
         self.train_dataset = dataset.skip(self.validation_samples)
-        self.train_dataset = self.train_dataset.prefetch(buffer_size=self.batch_size)
-        
+        self.train_dataset = self.train_dataset.prefetch(
+            buffer_size=self.batch_size)
+
     def __init_model(self):
         if (not os.path.isfile(self.model_path)):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT),
                                     self.model_path)
-        
         model = models.load_model(self.model_path)
         model.summary()
         return model
-    
+
     def __prepare_training(self):
         if self.shuffle:
-            self.dataset = self.dataset.shuffle(buffer_size=self.shuffle_buffer_size)
-        
+            self.dataset = self.dataset.shuffle(
+                buffer_size=self.shuffle_buffer_size)
+
     def train(self):
-        return self.model.fit(x=self.train_dataset, 
+        return self.model.fit(x=self.train_dataset,
                               epochs=self.epochs,
                               steps_per_epoch=self.steps_per_epoch,
                               validation_data=self.validation_dataset,
                               validation_steps=self.validation_steps,
                               verbose=2,
                               shuffle=False)
-    
+
     def print_dataset_batch(self):
         for iter in self.train_dataset:
             print(iter)
 
+
 def argument_parser():
     description = 'A tool to train a CNN using Keras/Tensorflow'
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-                                     description=description)
-    parser.add_argument("-d --dataset-path", dest="dataset_path", action="store",
-                        help="Set dataset path.")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description=description)
+    parser.add_argument("-d --dataset-path", dest="dataset_path",
+                        action="store", help="Set dataset path.")
     parser.add_argument("-m --model-path", dest="model_path", action="store",
                         default="", help="Set model path.")
-    parser.add_argument("--batch-size", dest="batch_size", type=int, default=10,
-                        help="Set batch size.")
+    parser.add_argument("--batch-size", dest="batch_size", type=int,
+                        default=10, help="Set batch size.")
     parser.add_argument("--epochs", dest="epochs", type=int, default=10,
                         help="Set training epochs.")
     parser.add_argument("--steps-per-epoch", dest="steps_per_epoch", type=int,
@@ -91,13 +98,16 @@ def argument_parser():
     parser.add_argument('--no-shuffle', dest="shuffle", action='store_false',
                         help="Do not shuffle the dataset.")
     parser.add_argument("--shuffle-buffer-size", dest="shuffle_buffer_size",
-                        type=int, default=10000, help="Set shuffle buffer size.")
+                        type=int, default=10000,
+                        help="Set shuffle buffer size.")
     parser.set_defaults(shuffle=True)
-    parser.add_argument("--split-ratio", default="0.8/0.2", nargs='+', dest="split_ratio",
-                        action="store", type=float,
-                        help='Set the train/validation portions. (Default: %(default)s)')
+    parser.add_argument("--split-ratio", default="0.8/0.2", nargs='+',
+                        dest="split_ratio", action="store", type=float,
+                        help='Set the train/validation portions. \
+                            (Default: %(default)s)')
     parser.add_argument("--validation-steps", dest="validation_steps",
-                        type=int, default=2, help="Set the number of validation steps.")
+                        type=int, default=2,
+                        help="Set the number of validation steps.")
     return parser
 
 
@@ -109,10 +119,10 @@ def main(trainer=signn_trainer, args=None):
                 epochs=args.epochs, steps_per_epoch=args.steps_per_epoch,
                 batch_size=args.batch_size, shuffle=args.shuffle,
                 shuffle_buffer_size=args.shuffle_buffer_size,
-                split_ratio=args.split_ratio, validation_steps=args.validation_steps)
+                split_ratio=args.split_ratio,
+                validation_steps=args.validation_steps)
     t.train()
-    
+
+
 if __name__ == '__main__':
     main()
-
-    
