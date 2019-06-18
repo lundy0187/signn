7fb9394b46ae6e920d7fbc3b651fa8c9d332cb28
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2019-06-18 15:46:00 +0300

    Add Tensorflow GPU restrictions Introduce script to download SatNOGS observations

--- signn_trainer.py
+++ signn_trainer.py
@@ -46,9 +46,22 @@ class signn_trainer():
         self.file_writer_cm = tf.summary.create_file_writer(
             self.logdir + '/cm')
         self.plotter = plt.plotter(artifacts_dest)
+        self.get_available_gpus()
 
-    def get_available_gpus():
+    def get_available_gpus(self):
         local_device_protos = device_lib.list_local_devices()
+
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+            # Restrict TensorFlow to only use the first GPU
+            try:
+                tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+            except RuntimeError as e:
+                # Visible devices must be set before GPUs have been initialized
+                print(e)
+
         return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
     def __init_dataset(self):
