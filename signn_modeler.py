2b2cc946f8f19968ed3993dbb9160c36fa79fd53
Kostis Triantafyllakis <kostastriantaf@gmail.com>
2019-07-03 17:57:14 +0300

    Append NN architectures to the signn_modeler
    *  Add DeepSig Inc. VGG architecture
    *  Add DeepSig Inc. Resnet architecture

--- signn_modeler.py
+++ signn_modeler.py
@@ -17,10 +17,14 @@
 
 import argparse
 import tensorflow.python.keras.models as models
+import tensorflow.keras.regularizers as regularizers
 from tensorflow.python.keras.layers.core import (Reshape, Dense, Dropout,
-                                                 Activation, Flatten)
+                                                 Activation, Flatten, Input,
+                                                 Add, BatchNormalization,
+                                                 AlphaDropout)
 from tensorflow.python.keras.layers.convolutional import (Conv2D,
-                                                          ZeroPadding2D)
+                                                          ZeroPadding2D,
+                                                          MaxPooling2D)
 
 
 class signn_modeler():
@@ -50,6 +54,10 @@ class signn_modeler():
     def __get_model(self):
         if (self.model_choice == "deepsig"):
             return self.__get_deepsig_cnn()
+        elif (self.model_choice == "deepsig_vgg"):
+            return self.__get_deepsig_vgg()
+        elif (self.model_choice == "deepsig_resnet"):
+            return self.__get_deepsig_resnet()
 
     def __get_deepsig_cnn(self):
         """
@@ -77,6 +85,146 @@ class signn_modeler():
         model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
         return model
 
+    def __get_deepsig_vgg(self, conv_layers=7, dense_layers=2, dr=0.5):
+        """
+        Return the model that describes the VGG CNN architecture as described
+        from Deepsig Inc.
+        """
+
+        model = models.Sequential()
+        model.add(Reshape(self.input_shape + [1],
+                          input_shape=self.input_shape))
+        model.add(ZeroPadding2D(padding=(0, 1)))
+        model.add(Conv2D(filters=64,
+                         kernel_size=(2, 3),
+                         activation="relu",
+                         name="conv0",
+                         padding="valid",
+                         kernel_initializer="glorot_uniform"))
+        model.add(MaxPooling2D(pool_size=(1, 2)))
+        model.add(Dropout(dr))
+
+        # add convolutional layers
+        for i in range(conv_layers-1):
+            model.add(ZeroPadding2D(padding=(0, 1)))
+            model.add(Conv2D(filters=64,
+                             kernel_size=(1, 3),
+                             activation="relu",
+                             padding="valid",
+                             kernel_initializer="glorot_uniform",
+                             name="conv" + str(i+1)))
+            model.add(MaxPooling2D(pool_size=(1, 2)))
+            model.add(Dropout(dr))
+
+        model.add(Flatten())
+
+        # add dense layers
+        for i in range(dense_layers):
+            model.add(Dense(128,
+                            activation='selu',
+                            kernel_initializer="he_normal",
+                            name="dense" + str(i)))
+            model.add(AlphaDropout(dr))
+
+        model.add(Dense(self.target_num, name="output"))
+        model.add(Activation('softmax'))
+        model.add(Reshape([self.target_num]))
+
+        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+        return model
+
+    def _residual_unit(self, inp, nb_channels, kernel_sizes, name):
+        shortcut = inp
+
+        nb_channels1, nb_channels2 = nb_channels
+
+        x = ZeroPadding2D(padding=(0, 1))(inp)
+        x = Conv2D(nb_channels1,
+                   kernel_sizes[0],
+                   padding='valid',
+                   use_bias=False,
+                   kernel_initializer="he_normal",
+                   kernel_regularizer=regularizers.l2(1.e-4),
+                   name=name + "_conv_1")(x)
+        # channels last
+        x = BatchNormalization(axis=3, name=name + "_bn_1")(x)
+        x = Activation('relu', name=name + "_act_1")(x)
+
+        x = ZeroPadding2D(padding=(0, 1))(x)
+        x = Conv2D(nb_channels2,
+                   kernel_sizes[1],
+                   padding='valid',
+                   use_bias=False,
+                   kernel_initializer="he_normal",
+                   kernel_regularizer=regularizers.l2(1.e-4),
+                   name=name + "_conv_2")(x)
+        # channels last
+        x = BatchNormalization(axis=3, name=name + "_bn_2")(x)
+        x = Add(name=name + "_add")([x, shortcut])
+        x = Activation('relu', name=name + "_act_2")(x)
+        return x
+
+    def _residual_stack(self, inp, nb_channels, kernel_sizes, name):
+        # 1x1 convolution
+        x = Conv2D(nb_channels[0],
+                   kernel_size=(1, 1),
+                   padding='valid',
+                   use_bias=False,
+                   kernel_initializer="he_normal",
+                   kernel_regularizer=regularizers.l2(1.e-4),
+                   name=name + "_conv_0")(inp)
+
+        x = self._residual_unit(x, nb_channels[1:3], kernel_sizes[0],
+                                name + "_resunit0")
+        x = self._residual_unit(x, nb_channels[1:3], kernel_sizes[1],
+                                name + "_resunit1")
+
+        x = MaxPooling2D(pool_size=(1, 2), name=name + "_mxpool_0")(x)
+        return x
+
+    def __get_deepsig_resnet(self, residual_stacks=6, dense_layers=2, dr=0.5):
+        """
+        Return the model that describes the ResNet architecture as described
+        from Deepsig Inc.
+        """
+
+        inp = Input(shape=self.input_shape)
+
+        x = Reshape(self.input_shape + [1])(inp)
+
+        x = ZeroPadding2D(padding=(0, 1))(x)
+        x = Conv2D(32,
+                   kernel_size=(2, 3),
+                   padding='valid',
+                   kernel_initializer="glorot_uniform",
+                   name="conv_0")(x)
+        # channels last
+        x = BatchNormalization(axis=3)(x)
+        x = Activation('relu')(x)
+
+        for i in range(residual_stacks):
+            x = self._residual_stack(x, nb_channels=[32, 32, 32],
+                                     kernel_sizes=[
+                                        [(1, 3), (1, 3)], [(1, 3), (1, 3)]],
+                                     name="resstack_" + str(i))
+
+        x = Flatten()(x)
+
+        for i in range(dense_layers):
+            x = Dense(128,
+                      activation='selu',
+                      kernel_initializer='he_normal',
+                      name="dense" + str(i))(x)
+            x = AlphaDropout(dr)(x)
+
+        x = Dense(self.target_num, name="output")(x)
+        x = Activation('softmax')(x)
+        x = Reshape([self.target_num])(x)
+        model = models.Model(inp, x, name='deepsig_resnet')
+        model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
+
+        return model
+
     def export_model(self):
         """
         Save the generated architecture.
@@ -96,7 +244,8 @@ def argument_parser():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=description)
     parser.add_argument("-m", "--model", default='deepsig', nargs='?',
-                        dest="model", action="store", choices=['deepsig'],
+                        dest="model", action="store",
+                        choices=['deepsig', 'deepsig_vgg', 'deepsig_resnet'],
                         help='Choose the model to generate. \
                             (Default: %(default)s)')
     parser.add_argument("-i", "--input-shape", dest="input_shape", nargs='+',
